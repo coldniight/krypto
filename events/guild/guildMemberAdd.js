86e068545bb08fca8574bf23af01eeecd4dd7675
coldniight <49598352+coldniight@users.noreply.github.com>
2019-06-02 23:47:56 -0500

    Delete guildMemberAdd.js

--- events/guild/guildMemberAdd.js
+++
@@ -1,6 +0,0 @@
-const { prefix } = require("../../botconfig.json")
-
-module.exports = async (bot, member) => {
-    var role = bot.guilds.get("577220184692097034").roles.find('name', 'Unverified');
-    member.addRole(role);
-}
